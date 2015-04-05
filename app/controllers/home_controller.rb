07bf2088369773dfb42d7c3f79856406024b44bb
davidbobeck <bobeckd@gmail.com>
2015-04-05 12:21:26 -0400

    neutered use of mongo and reds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    @toys = Toy.all
-    $redis.set('urn:sandbox:comment', 'This is a sweet comment from redis')
-    @comment = $redis.get('urn:sandbox:comment')
+    # @toys = Toy.all
+    # $redis.set('urn:sandbox:comment', 'This is a sweet comment from redis')
+    # @comment = $redis.get('urn:sandbox:comment')
+
+    @toys = []
+    @comment = "not using redis or mongo"
   end
 
 end
