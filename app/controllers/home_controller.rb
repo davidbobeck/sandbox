5fd86793b2583d5fc1260159a0e9085e16fa7c7a
davidbobeck <bobeckd@gmail.com>
2015-04-05 15:06:38 -0400

    restoring mongo & reds. database.yml is now blank.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    # @toys = Toy.all
-    # $redis.set('urn:sandbox:comment', 'This is a sweet comment from redis')
-    # @comment = $redis.get('urn:sandbox:comment')
-
-    @toys = []
-    @comment = "not using redis or mongo"
+    @toys = Toy.all
+    $redis.set('urn:sandbox:comment', 'This is a sweet comment from redis')
+    @comment = $redis.get('urn:sandbox:comment')
   end
 
 end
